661c528e21abe1620e677629c9b1cca295932c5e
MichelleMOU86 <Michelle.L.Munoz@gmail.com>
2025-04-05 23:02:19 -0400

    Added API integration and created functions to display behavior

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 function generatePoem(event) {
   event.preventDefault();
+
+  let poemField = document.querySelector("#poem");
+  poemField.innerHTML = "Getting your poem...please wait.";
+  let instructionInput = document.querySelector("#instruction");
+  let apiKey = "f86d964a4t362c8065oa46bbf2320366";
+  let context =
+    "You are an English historian and you know how to write beautiful poetry.";
+  let prompt = `Generate a short poem in English about ${instructionInput.value}. Separate each line with a line break.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showPoem);
+}
+
+function showPoem(response) {
   new Typewriter("#poem", {
-    strings: "Your poem goes here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: null,
